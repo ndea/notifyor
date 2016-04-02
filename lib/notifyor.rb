aaf11d5b5537b0f513a652a0965f3924038e4b6d
Erwin Schens <erwin.schens@qurasoft.de>
2016-04-02 10:18:54 +0200

    add redis-objects and add configuration

--- lib/notifyor.rb
+++ lib/notifyor.rb
@@ -1,2 +1,18 @@
 module Notifyor
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration) if block_given?
+  end
+
+  class Configuration
+    attr_accessor :redis_connection
+
+    def initialize
+      @redis_connection = Redis.new
+    end
+  end
 end
