8c977d5f18f5bb526bef206979dedbb61f4f11cf
Erwin Schens <erwin.schens@qurasoft.de>
2016-04-04 11:19:23 +0200

    modified connection class

--- bin/remote.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-SSH_HOST=$1;
-NOTIFYER_VALUE=$( ssh $SSH_HOST 'redis-cli LPOP notifyor');
-echo $NOTIFYER_VALUE;
--- lib/notifyor/remote/connection.rb
+++ lib/notifyor/remote/connection.rb
@@ -2,6 +2,14 @@ module Notifyor
   module Remote
     class Connection
       attr_accessor :ssh_host
+
+      def initialize(ssh_host)
+        @ssh_host = ssh_host
+      end
+
+      def retrieve_value
+        %x(ssh #{@ssh_host} 'redis-cli RPOP notifyor')
+      end
     end
   end
 end
