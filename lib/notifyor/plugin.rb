d8ddf0be27cbd482d9aca36deb46aafe190b58d1
Erwin Schens <erwin.schens@qurasoft.de>
2016-04-05 19:50:26 +0200

    add notifyor configuraiton options

--- lib/notifyor/plugin.rb
+++ lib/notifyor/plugin.rb
@@ -8,19 +8,45 @@ module Notifyor
     end
 
     module ClassMethods
-      attr_accessor :events
+      attr_accessor :notifyor_events
       attr_accessor :notifyor_models
 
       def notifyor(options = {})
+        configure_plugin(options)
         self.extend ::Redis::Objects
         ::Notifyor.configuration.notifyor_models.add(self.name)
-        self.events = ::Redis::List.new("notifyor:#{self.name.tableize}")
-        append_callbacks
+        self.notifyor_events = ::Redis::List.new("notifyor:#{self.name.tableize}")
       end
 
-      def append_callbacks
-        self.send(:before_create, -> { self.class.events << I18n.t('notifyor.model.create', model: self.model_name.human) })
-        self.send(:before_destroy, -> { self.class.events << I18n.t('notifyor.model.delete', model: self.model_name.human) })
+      def configure_plugin(options = {})
+        configuration = default_configuration.update(options)
+        append_callbacks(configuration)
+      end
+
+      def default_configuration
+        {
+            only: [:create, :destroy, :update],
+            messages: {
+                create: I18n.t('notifyor.model.create', model: self.model_name.human),
+                update: I18n.t('notifyor.model.update', model: self.model_name.human),
+                destroy: I18n.t('notifyor.model.destroy', model: self.model_name.human)
+            }
+        }
+      end
+
+      def append_callbacks(configuration)
+        configuration[:only].each do |action|
+          case action
+            when :create
+              self.after_commit -> { self.class.notifyor_events << configuration[:messages][:create] }, on: :create
+            when :update
+              self.after_commit -> { self.class.notifyor_events << configuration[:messages][:update] }, on: :update
+            when :destroy
+              self.before_destroy -> { self.class.notifyor_events << configuration[:messages][:destroy] }
+            else
+              #nop
+          end
+        end
       end
 
     end
