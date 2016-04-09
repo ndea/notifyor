58d095c0643a7989945457a90b4fb5c72e2c4dad
Erwin Schens <erwin.schens@qurasoft.de>
2016-04-09 09:38:27 +0200

    add option to notifyor to publish on other channels

--- lib/notifyor/plugin.rb
+++ lib/notifyor/plugin.rb
@@ -15,20 +15,23 @@ module Notifyor
 
       def configure_plugin(options = {})
         configuration = default_configuration.deep_merge(options)
-        append_callbacks(configuration)
+        publish_channels = configuration[:channels] || ['notifyor']
+        append_callbacks(configuration, publish_channels)
       end
 
-      def append_callbacks(configuration)
-        configuration[:only].each do |action|
-          case action
-            when :create
-              self.after_commit -> { ::Notifyor.configuration.redis_connection.publish "notifyor", configuration[:messages][:create].call(self) }, on: :create, if: -> { configuration[:only].include? :create }
-            when :update
-              self.after_commit -> { ::Notifyor.configuration.redis_connection.publish "notifyor", configuration[:messages][:update].call(self) }, on: :update, if: -> { configuration[:only].include? :update }
-            when :destroy
-              self.before_destroy -> { ::Notifyor.configuration.redis_connection.publish "notifyor", configuration[:messages][:destroy].call(self) }, if: -> { configuration[:only].include? :destroy }
-            else
-              #nop
+      def append_callbacks(configuration, publish_channels)
+        publish_channels.each do |channel|
+          configuration[:only].each do |action|
+            case action
+              when :create
+                self.after_commit -> { ::Notifyor.configuration.redis_connection.publish channel, configuration[:messages][:create].call(self) }, on: :create, if: -> { configuration[:only].include? :create }
+              when :update
+                self.after_commit -> { ::Notifyor.configuration.redis_connection.publish channel, configuration[:messages][:update].call(self) }, on: :update, if: -> { configuration[:only].include? :update }
+              when :destroy
+                self.before_destroy -> { ::Notifyor.configuration.redis_connection.publish channel, configuration[:messages][:destroy].call(self) }, if: -> { configuration[:only].include? :destroy }
+              else
+                #nop
+            end
           end
         end
       end
